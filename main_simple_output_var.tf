aea397f02d2bedbc41c90bf30a8bf0a5e8e085c9
uekawa-tomonari <16901713+uekawa-tomonari@users.noreply.github.com>
2023-06-06 09:47:48 +0900

    Update main_simple_output_var.tf

--- main_simple_output_var.tf
+++ main_simple_output_var.tf
@@ -1,1 +1,23 @@
-{a aa
+resource "random_pet" "pet" {
+        keepers = {
+                val = timestamp()
+        }
+}
+
+ 
+
+output "pet" {
+        value = random_pet.pet.id
+}
+
+
+
+
+
+variable "VAR_str" {
+    type = string
+    default = "doumo"
+}
+output "greeting" {
+  value = "${var.VAR_str}"
+}
